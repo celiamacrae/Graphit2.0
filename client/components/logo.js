8f72e76232147d3cb3539cf5a2657fb2dedc855e
Celia Macrae <celiamacrae@gmail.com>
2020-04-14 11:59:22 -0400

    bug fix, clears uploaded file and chosen graph

--- client/components/logo.js
+++ client/components/logo.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {clearGraph} from '../store/graph'
+import {gotUploadedFile} from '../store/upload'
 import {connect} from 'react-redux'
 
 class Logo extends React.Component {
+  clearUploadedFile = () => {
+    this.props.uploadFile(null)
+    this.props.clearGraph(null)
+  }
+
   render() {
     return (
       <Link to="/">
-        <div className="logo" onClick={this.props.clearChart}>
+        <div className="logo" onClick={this.clearUploadedFile}>
           Graph<span className="it">it</span>
         </div>
       </Link>
@@ -15,12 +21,9 @@ class Logo extends React.Component {
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    clearChart: function() {
-      dispatch(clearGraph())
-    }
-  }
-}
+const mapDispatchToProps = dispatch => ({
+  uploadFile: file => dispatch(gotUploadedFile(file)),
+  clearGraph: () => dispatch(clearGraph())
+})
 
 export default connect(null, mapDispatchToProps)(Logo)

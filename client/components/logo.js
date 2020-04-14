bd0aac54aa432848e392e018e0b356ed291a8ed2
Celia Macrae <celiamacrae@gmail.com>
2020-04-14 11:52:16 -0400

    bug fix-- graph is unsaved

--- client/components/logo.js
+++ client/components/logo.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {clearGraph} from '../store/graph'
+import {connect} from 'react-redux'
 
-const Logo = () => {
-  return (
-    <Link to="/">
-      <div className="logo">
-        Graph<span className="it">it</span>
-      </div>
-    </Link>
-  )
+class Logo extends React.Component {
+  render() {
+    return (
+      <Link to="/">
+        <div className="logo" onClick={this.props.clearChart}>
+          Graph<span className="it">it</span>
+        </div>
+      </Link>
+    )
+  }
 }
-export default Logo
+
+const mapDispatchToProps = dispatch => {
+  return {
+    clearChart: function() {
+      dispatch(clearGraph())
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Logo)
